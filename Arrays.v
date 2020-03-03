8886ad9e0f59e548e738d0e99a2fc01bcb30d48c
mekty2012 <41375658+mekty2012@users.noreply.github.com>
2020-03-03 23:48:58 +0900

    Update Arrays.v
    Add specific implementation of ArLang.

--- Arrays.v
+++ Arrays.v
@@ -29,36 +29,83 @@ Inductive bexp : Type :=
   | BNot (b : bexp)
   | BAnd (b1 b2 : bexp).
 
-(*
-Fixpoint aeval (st : state) (a : aexp) : nat :=
+Definition state := total_map nat.
+Definition arstate := total_map (nat -> nat).
+
+Fixpoint aeval (st : state) (arst : arstate) (a : aexp) : nat :=
   match a with
   | ANum n => n
   | AId x => st x
-  | APlus a1 a2 => (aeval st a1) + (aeval st a2)
-  | AMinus a1 a2  => (aeval st a1) - (aeval st a2)
-  | AMult a1 a2 => (aeval st a1) * (aeval st a2)
-  
+  | APlus a1 a2 => (aeval st arst a1) + (aeval st arst a2)
+  | AMinus a1 a2  => (aeval st arst a1) - (aeval st arst a2)
+  | AMult a1 a2 => (aeval st arst a1) * (aeval st arst a2)
+  | AArray x a1 => (arst x) (aeval st arst a1)
   end.
 
-Fixpoint beval (st : state) (b : bexp) : bool :=
+Fixpoint beval (st : state) (arst : arstate) (b : bexp) : bool :=
   match b with
   | BTrue       => true
   | BFalse      => false
-  | BEq a1 a2   => (aeval st a1) =? (aeval st a2)
-  | BLe a1 a2   => (aeval st a1) <=? (aeval st a2)
-  | BNot b1     => negb (beval st b1)
-  | BAnd b1 b2  => andb (beval st b1) (beval st b2)
+  | BEq a1 a2   => (aeval st arst a1) =? (aeval st arst a2)
+  | BLe a1 a2   => (aeval st arst a1) <=? (aeval st arst a2)
+  | BNot b1     => negb (beval st arst b1)
+  | BAnd b1 b2  => andb (beval st arst b1) (beval st arst b2)
   end.
-*)
 
 Inductive ArLang: Type :=
-  | SSkip : ArLang
-  | SAssn (x:string) (a:aexp) : ArLang
-  | SSeq (first:ArLang) (second:ArLang) : ArLang
-  | SIf (cond:bexp) (thenE:ArLang) (elseE:ArLang) : ArLang
-  | SWhile (cond:bexp) (body:ArLang) : ArLang
-  | SArrAssn (x : string) (a1 a2:aexp) : ArLang
-  | SNewArray (x : string) (afrom ato ainit:aexp) (body:ArLang) : ArLang.
+  | ArSkip : ArLang
+  | ArAssn (x:string) (a:aexp) : ArLang
+  | ArSeq (first:ArLang) (second:ArLang) : ArLang
+  | ArIf (cond:bexp) (thenE:ArLang) (elseE:ArLang) : ArLang
+  | ArWhile (cond:bexp) (body:ArLang) : ArLang
+  | ArArrAssn (x : string) (a1 a2:aexp) : ArLang
+  | ArNewArray (x : string) (afrom ato ainit:aexp) (body:ArLang) : ArLang.
 
-Definition state := total_map nat.
-Definition arstate := total_map (list nat).
+Definition new_array (n1 n2 n3: nat) : (nat -> nat) :=
+  fun (t:nat) => 
+  if (andb (t <=? n2) (n1 <=? n1)) then n3
+  else 0.
+
+Definition n_update (n1 n2:nat) (m:(nat -> nat)) : (nat -> nat) :=
+  fun t => if (t =? n1) then n2 else (m t).
+
+Inductive areval : ArLang -> state -> arstate -> state -> arstate -> Prop :=
+  | ArE_Skip :    forall st arst,
+      areval ArSkip st arst st arst
+  | ArE_Ass : forall st a1 n x arst,
+      aeval st arst a1 = n ->
+      areval (ArAssn x a1) st arst (x !-> n ; st) arst
+  | ArE_Seq : forall c1 c2 st st' st'' arst arst' arst'',
+      areval c1 st arst st' arst' ->
+      areval c2 st' arst' st'' arst'' ->
+      areval (ArSeq c1 c2) st arst st'' arst''
+  | ArE_IfTrue : forall st st' b c1 c2 arst arst',
+      beval st arst b = true ->
+      areval c1 st arst st' arst' ->
+      areval (ArIf b c1 c2) st arst st' arst'
+  | ArE_IfFalse : forall st st' b c1 c2 arst arst',
+      beval st arst b = false ->
+      areval c2 st arst st' arst' ->
+      areval (ArIf b c1 c2) st arst st' arst'
+  | ArE_WhileFalse : forall b st c arst,
+      beval st arst b = false ->
+      areval (ArWhile b c) st arst st arst
+  | ArE_WhileTrue : forall st st' st'' b c arst arst' arst'',
+      beval st arst b = true ->
+      areval c st arst st' arst' ->
+      areval (ArWhile b c) st' arst' st'' arst'' ->
+      areval (ArWhile b c) st arst st'' arst''
+  | ArE_ArrAssn : forall st arst x a1 a2 n1 n2,
+      aeval st arst a1 = n1 ->
+      aeval st arst a2 = n2 ->
+      areval (ArArrAssn x a1 a2) st arst st (x !-> (n_update n1 n2 (arst x)); arst)
+  | ArE_NewArray : forall st st' arst arst' x a1 a2 a3 c1 n1 n2 n3,
+      aeval st arst a1 = n1 ->
+      aeval st arst a2 = n2 ->
+      aeval st arst a3 = n3 ->
+      areval c1 st (x !-> (new_array n1 n2 n3); arst) st' arst' ->
+      areval (ArNewArray x a1 a2 a3 c1) st arst st' arst'.
+
+Notation "st '/' arst '=[' c ']=>' st2 '/' arst2" := (areval c st arst st2 arst2)
+  (at level 40).
+
